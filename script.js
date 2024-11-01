6da3389aa8aa1dc32b7a3ff41f1ce216cf15d0cc
Rafael <rafael.mastellini@alunoescolassociais.org.br>
2024-11-01 09:43:24 -0300

    adiciona todas as informações atualizadas ao readme

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const apiKey = '687da2f1a2edb71b95661776';
 
 const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+// Função para buscar taxa de câmbio via API
+async function getExchangeRate(daMoeda, ParaMoeda) {
+    try{
+        const response = await fetch(`${apiURL}${daMoeda}`);
+        const data = await response.json();
+
+        if(data.result == "success"){
+            return data.conversion_rates[ParaMoeda];
+        }else{
+            throw new Error('Erro ao buscar as taxas de câmbio');
+        }
+
+    }catch (error){
+
+    }
+}
